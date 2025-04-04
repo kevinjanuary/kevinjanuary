928e7fab3b6dbf624cb14d59da75759d63f1796c
Kevin Januari <128971428+kevinjanuary@users.noreply.github.com>
2025-04-04 11:14:59 +0700

    don't look, there's nothing here 😅

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+export default async function middleware(req: NextRequest) {
+  const url = req.nextUrl
+
+  const hostname = req.headers.get("host")
+
+  console.log("--- Middleware ---")
+  console.log("Header host =>", hostname)
+  console.log("Request URL =>", url)
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /_static (inside /public)
+     * 4. all root files inside /public (e.g. /favicon.ico)
+     */
+    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
+  ],
+}
